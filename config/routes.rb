9eda4972d0928aade408cbb41d4f7e1e410aaadd
andreavsotoa <asoto999@hotmail.com>
2017-04-14 15:02:13 -0400

    Se agregó login del dashboard con estilos

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  # Se cambio ruta raiz app para que cargue la pagina del index
+  #get 'home/index'
+  root 'home#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
