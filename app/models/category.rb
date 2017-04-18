665fcde162a7ed6f9806ac2114265f65b2541de0
andreavsotoa <asoto999@hotmail.com>
2017-04-18 13:42:45 -0400

    Agregando dataTables al recurso categorías: FALTA

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ApplicationRecord
 
+	has_many :subcategories
+	
 	validates :nombre, presence: { message: "Debe colocar el nombre de la categoría" }
  
 	validates :descripcion, presence: { message: "Debe colocar la descripción de la categoría" }
