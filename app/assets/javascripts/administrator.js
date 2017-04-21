2038e3a119d9b055496e87e1a68009d16a9579b8
andreavsotoa <asoto999@hotmail.com>
2017-04-21 13:49:42 -0400

    Arreglos en la vista index de categories

--- app/assets/javascripts/administrator.js
+++ app/assets/javascripts/administrator.js
@@ -1,5 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require plugins/dataTables/jquery.dataTables
+//= require plugins/dataTables/dataTables.bootstrap
 //= require turbolinks
 //= require_self
